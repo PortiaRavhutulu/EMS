572414fe2f9419f9b68683bdd7aa3191e8c4be60
PortiaRavhutulu <ravhutuluportia@gmail.com>
2023-04-24 14:44:56 +0200

    I refined the form, added validation as well as the clear all fields button

--- src/app/Components/create/create.component.ts
+++ src/app/Components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent {
+  userForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    // Create the form group
+    this.userForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      department: ['', Validators.required],
+      salary: ['', Validators.required],
+      cellphone: ['', Validators.required],
+      address: ['', Validators.required],
+      suburb: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      postalCode: ['', Validators.required],
+      startDate: ['', Validators.required],
+      
+    });
+  }
+
+  ngOnInit(): void { }
+
+  clearInput() {
+    // Clear the input field
+    this.userForm.get('firstName')?.reset();
+    this.userForm.get('lastName')?.reset();
+    this.userForm.get('email')?.reset();
+    this.userForm.get('department')?.reset();
+    this.userForm.get('cellphone')?.reset();
+    this.userForm.get('salary')?.reset();
+    this.userForm.get('address')?.reset();
+    this.userForm.get('suburb')?.reset();
+    this.userForm.get('city')?.reset();
+    this.userForm.get('country')?.reset();
+    this.userForm.get('postalCode')?.reset();
+    this.userForm.get('startDate')?.reset();
+    console.log("cleared!")
+  }
 
 }

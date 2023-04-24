0d447d2e3a9aafbac270de691781be0fb038bdfa
PortiaRavhutulu <ravhutuluportia@gmail.com>
2023-04-24 11:46:00 +0200

    I fixed an minor error I was having

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateComponent } from './create-employee/create-employee.component';
+import { CreateComponent } from './Components/create/create.component';
 
 const routes: Routes = [
   { path: '', component:CreateComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { CreateComponent } from './Components/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateEmployeeComponent,
     CreateComponent
   ],
   imports: [

e3df296ffe713558ca3976429fc233ab7bc483f0
PortiaRavhutulu <ravhutuluportia@gmail.com>
2023-04-25 08:34:02 +0200

    I created the routing paths for the current components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './Components/create/create.component';
 import { UpdateComponent } from './Components/update/update.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { EmployeeComponent } from './Components/employee/employee.component';
+
 
 
 const routes: Routes = [
   { path: 'create', component:CreateComponent },
   { path: 'update', component: UpdateComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'employee', component: EmployeeComponent },
 ];
 
 @NgModule({

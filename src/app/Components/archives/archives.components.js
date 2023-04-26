965b780ca40839440e13dfb7bb6025642ae1d719
Bolebo67 <bolebo7@gmail.com>
2023-04-26 13:38:54 +0200

    lebo 2nd commit

---
+++ src/app/Components/archives/archives.components.js
@@ -0,0 +1,20 @@
+document.querySelector('#tableID');
+
+element.addEventListener('click', handler);
+
+const table = document.querySelector('#tableID');
+const rows = table.tBodies[0].rows;
+
+Array.from(rows).forEach((row, idx) => {
+    row.addEventListener('click', event => {
+        const tds = Array.from(row.cells).map(td => td.textContent);
+
+        console.clear();
+
+        console.log('row index:', idx);
+
+        console.log('tds content:', ...tds);
+
+        console.log('tds string:', tds.join(' '));
+    });
+  });

b8bd2f976db6dfdeff4c35c0f9ad8a6ce82b02d9
Bolebo67 <bolebo7@gmail.com>
2023-05-02 12:08:31 +0200

    lebo

---
+++ src/app/Components/archives/table.components.ts
@@ -0,0 +1,17 @@
+
+const table = document.querySelector('#tableID');
+const rows = table.tBodies[0].rows;
+
+Array.from(rows).forEach((row, idx) => {
+    row.addEventListener('click', event => {
+        const tds = Array.from(row.cells).map(td => td.textContent);
+
+        console.clear();
+
+        console.log('row index:', idx);
+
+        console.log('tds content:', ...tds);
+
+        console.log('tds string:', tds.join(' '));
+    });
+  });

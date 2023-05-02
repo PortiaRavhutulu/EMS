05ecfdddddb600f87c95c089235407638aaba61e
Bolebo67 <bolebo7@gmail.com>
2023-05-02 13:14:44 +0200

    TB

--- src/app/Components/archives/archives.components.js
+++ src/app/Components/archives/archives.components.js
@@ -1,21 +1,17 @@
-document.querySelector('#tableID');
-
-table.querySelectorAll('tbody td');
-element.addEventListener('click', handler);
-
-const table = document.querySelector('#tableID');
-const rows = table.tBodies[0].rows;
-
-Array.from(rows).forEach((row, idx) => {
-    row.addEventListener('click', event => {
-        const tds = Array.from(row.cells).map(td => td.textContent);
-
-        console.clear();
-
-        console.log('row index:', idx);
-
-        console.log('tds content:', ...tds);
-
-        console.log('tds string:', tds.join(' '));
-    });
-  });
+$(document).ready(function() {
+    $('#mytable').DataTable( {
+        dom: 'Bfrtip',
+        columnDefs: [
+            {
+                targets: 1,
+                className: 'noVis'
+            }
+        ],
+        buttons: [
+            {
+                extend: 'colvis',
+                columns: ':not(.noVis)'
+            }
+        ]
+    } );
+} );
